c3033bbd1c3d1a379d543fc8ad51927ebe53e12c
muhammadtaha751 <121214442+muhammadtaha751@users.noreply.github.com>
2024-02-21 17:09:54 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Navigation from './src/config/Navigation'
+import 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+   <Navigation/>
+  );
+}
+
